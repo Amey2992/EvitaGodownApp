3ceb056251d50a986febffdbc4c5d495e4cbcb43
sachin wadekar <wadekar.sachin100@gmail.com>
2018-10-16 10:47:44 +0530

    package created

---
+++ app/src/main/java/com/commercialMgmt/AddNewConsumer.java
@@ -0,0 +1,15 @@
+package com.commercialMgmt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.infosolutions.evita.R;
+
+public class AddNewConsumer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_consumer);
+    }
+}
